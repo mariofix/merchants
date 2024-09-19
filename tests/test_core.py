e46d7e5fd0653475e9ed71776d8b4e27659603cd
Mario Hernandez <mariofix@proton.me>
2024-09-19 06:11:54 -0300

    fix test

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,19 +1,10 @@
-import pytest
+import pytest  # noqa
 from fastapi.testclient import TestClient
-from sqlmodel import Session
 
-from merchants.database import engine
 from merchants.FastapiApp import app
 
 # Create a test client
 client = TestClient(app)
-
-
-# Mock database session
-@pytest.fixture
-def mock_db():
-    with Session(engine) as session:
-        yield session
 
 
 # Test root endpoint
